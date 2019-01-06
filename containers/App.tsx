2a931d833673a21331b34b8f2fcc73bd1db3590f
Valeriy Saphonov <xxrrrxx@yandex.ru>
2019-01-06 09:48:27 +0300

    first commit

---
+++ containers/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Title from "../components/Title";
+
+export const App: React.FunctionComponent = () => (
+  <div>
+    <Title titleColor="red" />
+
+    <span>default</span>
+  </div>
+);
---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { App } from "../containers/App";
+
+const Index = () => <App />;
+
+export default Index;
